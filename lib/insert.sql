8d427d69a96d3cd2bb00e8d80e005f8df1c03166
moniet <moniet.sawhney@gmail.com>
2019-05-22 14:01:23 +0100

    first solution

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 5, "male", "blue", "calm", "no"),
+(2,"Rowdy", 6, "female", "red", "angry", "yes"),
+(3,"Tabitha", 5, "male", "blue", "calm", "no"),
+(4,"Sergeant Brown", 5, "male", "red", "calm", "no"),
+(5,"Melissa", 5, "male", "brown", "calm", "no"),
+(6,"Grinch", 5, "female", "black", "calm", "no"),
+(7,"Wendy", 5, "male", "blue", "calm", "no"),
+(8,NULL, 5, "female", "red", "calm", "no");
