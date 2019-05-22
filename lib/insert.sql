968e70df9b147a56446340258594a5849cadb62e
flexkat <catherinemaritz1@gmail.com>
2019-05-22 10:06:53 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate
+", 12, "Male", "Brown", "Calm", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 11, "Male", "Golden", "Rough", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 11, "Female", "Black", "Calm", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "Male", "Brown", "Calm", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 9, "Female", "Brown", "Calm", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 11, "Male", "Brown", "Angry", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "Feale", "Brown", "Calm", true);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 11, "Male", "Brown", "Angry", true);
