3bf6997695ee37df835de58704624fe3b0b295fc
moniet <moniet.sawhney@gmail.com>
2019-05-22 15:07:39 +0100

    Solution

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 5, "male", "blue", "calm", "no"),
-(2,"Rowdy", 6, "female", "red", "angry", "yes"),
-(3,"Tabitha", 5, "male", "blue", "calm", "no"),
-(4,"Sergeant Brown", 5, "male", "red", "calm", "no"),
-(5,"Melissa", 5, "male", "brown", "calm", "no"),
-(6,"Grinch", 5, "female", "black", "calm", "no"),
-(7,"Wendy", 5, "male", "blue", "calm", "no"),
-(8,NULL, 5, "female", "red", "calm", "no");
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
