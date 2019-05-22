fa405203510a5ea10358205a5e02e0f7f498d409
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-22 15:24:04 +0100

    completed test specs

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, "male", "brown", "smooth", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 7, "male", "cream", "aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 3, "female", "white", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 9, "male", "golden", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 7, "female", "black", "smooth", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 6, "male", "brown", "moody", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 4, "female", "golden", "moody", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 3, "female", "white", "cautious", true);
