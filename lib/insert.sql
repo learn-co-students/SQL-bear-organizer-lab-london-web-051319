028726058761e3c129ff402baafbbb52c58c9168
Amy Quinn <amyequinn@hotmail.co.uk>
2019-05-23 11:42:26 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, gender) VALUES ('Mr. Chocolate', 'female');
+INSERT INTO bears (name, gender) VALUES ('Rowdy', 'female');
+INSERT INTO bears (name, gender) VALUES ('Tabitha', 'male');
+INSERT INTO bears (name, gender) VALUES ('Sergeant Brown', 'male');
+INSERT INTO bears (name, gender) VALUES ('Melissa', 'female');
+INSERT INTO bears (name, gender) VALUES ('Grinch', 'female');
+INSERT INTO bears (name, gender) VALUES ('Wendy', 'male');
+INSERT INTO bears (name) VALUES (NULL);
